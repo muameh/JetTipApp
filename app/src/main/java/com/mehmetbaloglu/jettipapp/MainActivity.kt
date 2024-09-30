914e33ebff0ce0457882e9dff5554bb3e809b342
muameh <balmehmet111@gmail.com>
2024-09-30 11:38:00 +0300

    completed witout videmodel

--- app/src/main/java/com/mehmetbaloglu/jettipapp/MainActivity.kt
+++ app/src/main/java/com/mehmetbaloglu/jettipapp/MainActivity.kt
@@ -8,7 +8,6 @@ import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
@@ -51,7 +50,6 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.mehmetbaloglu.jettipapp.ui.theme.JetTipAppTheme
-import kotlin.math.sin
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -59,9 +57,7 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             JetTipAppTheme {
-                MyApp {
-
-                }
+                MyApp { }
             }
         }
     }
@@ -69,87 +65,141 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun MyApp(content: @Composable () -> Unit) {
+    val perPerson = remember { mutableStateOf(0.0) }
     Column(
         modifier = Modifier.fillMaxSize(),
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally
-    )
-    {
-        TopHeader()
-        MainContent()
+    ) {
+        TopHeader(perPerson)
+        MainContent(perPerson)
     }
 }
 
 @Composable
-fun MainContent() {
-    val valueState = remember { mutableStateOf("") }
-    val sliderPositionState = remember { mutableStateOf(0f) }
-    val focusManager = LocalFocusManager.current
+fun TopHeader(perPerson: MutableState<Double>) {
     Surface(
         modifier = Modifier
-            .padding(2.dp)
             .fillMaxWidth()
-            .clickable { focusManager.clearFocus()},
-        shape = RoundedCornerShape(corner = CornerSize(8.dp)),
-        border = BorderStroke(width = 2.dp, color = Color.LightGray)
+            .height(150.dp)
+            .clip(shape = CircleShape.copy(all = CornerSize(30.dp)))
+            .padding(5.dp),
+        color = Color(0xFFE9D7F7)
     ) {
-        Column {
-            EnterBill(valueState)
-            SplitRow()
-            TipRow()
-            Column(
-                modifier = Modifier.fillMaxWidth(),
-                horizontalAlignment = Alignment.CenterHorizontally,
-                verticalArrangement = Arrangement.Center
-            ) {
-                Text(text = "${sliderPositionState.value}")
-                Spacer(modifier = Modifier.height(14.dp))
-                Slider(
-                    modifier = Modifier.padding(start = 16.dp, end = 16.dp),
-                    steps = 5,
-                    valueRange = 0f..100f,
-                    value = sliderPositionState.value,
-                    onValueChange = {
-                        sliderPositionState.value = it // Slider pozisyonunu güncelle
-                        Log.d("Slider", "MainContent: $it")},
-                    onValueChangeFinished = {
-                        Log.d("Slider", "MainContent: Finished")
-                    }
-                )
-
-            }
+        Column(
+            modifier = Modifier.padding(5.dp),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center
+        ) {
+            val total = "%.2f".format(perPerson.value)
+            Text(
+                text = "Total Per Person",
+                modifier = Modifier.padding(5.dp),
+                style = MaterialTheme.typography.headlineMedium
+            )
+            Text(
+                text = "$$total",
+                style = MaterialTheme.typography.displaySmall,
+                fontWeight = FontWeight.ExtraBold
+            )
         }
     }
 }
 
 @Composable
-private fun TipRow() {
+fun MainContent(perPerson: MutableState<Double>) {
+    val valueState = remember { mutableStateOf("") }
+    val sliderPositionState = remember { mutableStateOf(0) }
+    val focusManager = LocalFocusManager.current
+    val splitValue = remember { mutableStateOf(1) }
+    val tipAmountState = remember { mutableStateOf("") }
+
+    // Calculate the tip amount and update tipAmountState
+    val tipAmount = CalculateTipAmount(
+        valueState.value.toDoubleOrNull() ?: 0.0, sliderPositionState.value.toDouble()
+    )
+    tipAmountState.value = "%.2f".format(tipAmount)
+
+    // Calculate the result and update perPerson
+    val result = CalculationPerPerson(
+        splitValue.value,
+        valueState.value.toDoubleOrNull() ?: 0.0,
+        sliderPositionState.value.toDouble()
+    )
+    perPerson.value = result // perPerson'ı result'a eşitle
+
+    Surface(
+        modifier = Modifier
+            .padding(5.dp)
+            .fillMaxWidth()
+            .clickable { focusManager.clearFocus() },
+        shape = RoundedCornerShape(corner = CornerSize(8.dp)),
+        border = BorderStroke(width = 2.dp, color = Color.LightGray)
+    ) {
+        Column(modifier = Modifier.padding(5.dp)) {
+            EnterBill(valueState)
+            SplitRow(splitValue)
+            TipRow(tipAmountState)
+            SliderColumn(sliderPositionState)
+        }
+    }
+}
+
+@Composable
+private fun SliderColumn(sliderPositionState: MutableState<Int>) {
+    Column(
+        modifier = Modifier.fillMaxWidth(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        Text(text = "${sliderPositionState.value.toInt()}%")
+        Spacer(modifier = Modifier.height(14.dp))
+        Slider(modifier = Modifier.padding(start = 16.dp, end = 16.dp),
+            valueRange = 0f..100f,
+            value = sliderPositionState.value.toFloat(),
+            onValueChange = {
+                sliderPositionState.value = it.toInt()
+            },
+            onValueChangeFinished = {
+                Log.d("Slider", "MainContent: Finished")
+            })
+    }
+}
+
+@Composable
+private fun TipRow(tipAmount: MutableState<String>) {
     Row(modifier = Modifier.padding(10.dp)) {
         Text(text = "Tip")
         Text(
-            text = "$33.00",
-            modifier = Modifier.padding(start = 195.dp)
+            text = "$${tipAmount.value}", modifier = Modifier.padding(start = 195.dp)
         )
     }
 }
+
+private fun CalculateTipAmount(totalBill: Double, tipPercentage: Double): Double {
+    return totalBill * tipPercentage / 100
+}
+
+private fun CalculationPerPerson(
+    splitAmount: Int, totalBill: Double, tipPercentage: Double
+): Double {
+    val tipAmount = totalBill * tipPercentage / 100
+    return (totalBill + tipAmount) / splitAmount
+}
+
 
 @Composable
 private fun EnterBill(valueState: MutableState<String>) {
     val focusManager = LocalFocusManager.current
     OutlinedTextField(
         value = valueState.value,
-        onValueChange = {
-            if (it.all { it.isDigit() }) {
-                valueState.value = it
-            }
-        },
+        onValueChange = { valueState.value = it },
         label = { Text("Enter Bill") },
         modifier = Modifier
             .padding(10.dp)
             .fillMaxWidth(),
         keyboardOptions = KeyboardOptions(
-            keyboardType = KeyboardType.Number,
-            imeAction = ImeAction.Done
+            keyboardType = KeyboardType.Number, imeAction = ImeAction.Done
         ),
         leadingIcon = {
             Icon(
@@ -159,22 +209,17 @@ private fun EnterBill(valueState: MutableState<String>) {
             )
         },
         textStyle = TextStyle(
-            fontSize = 18.sp,
-            color = MaterialTheme.colorScheme.onBackground
+            fontSize = 18.sp, color = MaterialTheme.colorScheme.onBackground
         ),
         singleLine = true,
-        keyboardActions = KeyboardActions(
-            onDone = {
-                focusManager.clearFocus()
-            }
-        )
-
-
+        keyboardActions = KeyboardActions(onDone = {
+            focusManager.clearFocus()
+        })
     )
 }
 
 @Composable
-private fun SplitRow() {
+private fun SplitRow(sliderPositionState: MutableState<Int>) {
     Row(modifier = Modifier.padding(3.dp)) {
         Text(
             text = "Split",
@@ -190,7 +235,7 @@ private fun SplitRow() {
             shadowElevation = 4.dp // Butona gölge efekti ekle
         ) {
             IconButton(
-                onClick = { /* Butona tıklanınca yapılacak işlemler */ },
+                onClick = { sliderPositionState.value++ },
                 modifier = Modifier.align(Alignment.CenterVertically)
             ) {
                 Icon(
@@ -201,25 +246,30 @@ private fun SplitRow() {
             }
         }
         Text(
-            text = "2",
+            text = sliderPositionState.value.toString(),
             modifier = Modifier
                 .align(alignment = Alignment.CenterVertically)
                 .padding(start = 10.dp, end = 10.dp)
         )
         Surface(
-            modifier = Modifier.size(40.dp), // Butonun boyutunu ayarla
-            shape = CircleShape, // Yuvarlak bir buton yapısı
-            color = Color.White, // Arka plan rengi beyaz
-            shadowElevation = 4.dp // Butona gölge efekti ekle
+            modifier = Modifier.size(40.dp),
+            shape = CircleShape,
+            color = Color.White,
+            shadowElevation = 4.dp
         ) {
             IconButton(
-                onClick = { /* Butona tıklanınca yapılacak işlemler */ },
-                modifier = Modifier.align(Alignment.CenterVertically)
+                onClick = {
+                    if (sliderPositionState.value > 1) {
+                        sliderPositionState.value--
+                    } else {
+                        sliderPositionState.value = 1
+                    }
+                }, modifier = Modifier.align(Alignment.CenterVertically)
             ) {
                 Icon(
                     imageVector = Icons.Default.Remove,
                     contentDescription = "Add Icon",
-                    tint = Color.Black // İkonun rengi siyah
+                    tint = Color.Black
                 )
             }
         }
@@ -227,37 +277,6 @@ private fun SplitRow() {
     }
 }
 
-
-@Composable
-fun TopHeader(totalPerPerson: Double = 0.0) {
-    Surface(
-        modifier = Modifier
-            .fillMaxWidth()
-            .height(150.dp)
-            .clip(shape = CircleShape.copy(all = CornerSize(30.dp)))
-            .padding(10.dp),
-        color = Color(0xFFE9D7F7)
-    ) {
-        Column(
-            modifier = Modifier.padding(5.dp),
-            horizontalAlignment = Alignment.CenterHorizontally,
-            verticalArrangement = Arrangement.Center
-        ) {
-            val total = "%.2f".format(totalPerPerson)
-            Text(
-                text = "Total Per Person",
-                modifier = Modifier.padding(5.dp),
-                style = MaterialTheme.typography.headlineMedium
-            )
-            Text(
-                text = "$$total",
-                style = MaterialTheme.typography.displaySmall,
-                fontWeight = FontWeight.ExtraBold
-            )
-        }
-
-    }
-}
 
 @Preview
 @Composable
